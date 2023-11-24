5a7277226e5a5bfbaddb82e7e8f1158ec916d844
Ynverxe <nahubar65@gmail.com>
2023-11-24 09:34:59 -0300

    docs: add some docs

--- src/main/java/com/github/ynverxe/binarysections/view/BinaryStackHeader.java
+++ src/main/java/com/github/ynverxe/binarysections/view/BinaryStackHeader.java
@@ -7,10 +7,21 @@ import java.nio.ByteBuffer;
 
 public interface BinaryStackHeader {
 
+  /**
+   * The length of every payload section.
+   */
   int maxPayloadLength();
 
+  /**
+   * The number of sections written in the stack data.
+   */
   int writtenSections();
 
+  /**
+   * The length of a complete section, this length is four bytes for an integer number
+   * denoting the used payload count followed by the entire payload length.
+   * This value is not present into the stack data, it's just a utility method.
+   */
   default int sectionLength() {
     return BinaryStackFormat.MAX_PAYLOAD_SIZE_LENGTH + maxPayloadLength();
   }
